21e94474bade82dcc6ee407a46261a23761accd0
Sons123 <100420581+Sons123@users.noreply.github.com>
2023-11-04 18:54:14 +0000

    Calculating Trajectories (p1, p2, p3) (ocorreu o mesmo que na aceleração, com a diferença de que não foi necessário ajustar nenhum valor)

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Drive : MonoBehaviour {
-    public float speed = 1.0f;
+    public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public Transform transGun;
     public Transform gun;
@@ -27,16 +27,13 @@ public class Drive : MonoBehaviour {
         // Rotate around our y-axis
         transform.Rotate(0, rotation, 0);
 
-        if (Input.GetKey(KeyCode.T)) 
-        {
+        if (Input.GetKey(KeyCode.T)) {
 
             transGun.RotateAround(transGun.position, transGun.right, -2.0f);
-        } else if (Input.GetKey(KeyCode.G)) 
-        {
+        } else if (Input.GetKey(KeyCode.G)) {
 
             transGun.RotateAround(transGun.position, transGun.right, 2.0f);
-        } else if (Input.GetKeyDown(KeyCode.B)) 
-        {
+        } else if (Input.GetKeyDown(KeyCode.B)) {
 
             Instantiate(bulletObj, gun.position, gun.rotation);
         }
--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -7,8 +7,8 @@ public class Shell : MonoBehaviour {
     public GameObject explosion;
     float speed = 0.0f;
     float ySpeed = 0.0f;
-    float mass = 10.0f;
-    float force = 2.0f;
+    float mass = 30.0f;
+    float force = 4.0f;
     float drag = 1.0f;
     float gravity = -9.8f;
     float gAccel;
